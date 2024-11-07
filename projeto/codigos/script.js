8f643df00b6e62acbf57260e5f85d1687ee59629
Guilherme Machado <157429638+Guilherme-axt@users.noreply.github.com>
2024-11-07 13:36:40 -0300

    Add files via upload

--- projeto/codigos/script.js
+++ projeto/codigos/script.js
@@ -1,6 +1,11 @@
+//JAVASCRIPT FUNCIONAL
+
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 const botaoIniciar = document.getElementById('start');
+const botaoReiniciar = document.getElementById('restart');
+
+
 
 
 
@@ -13,6 +18,10 @@ const jump = () => {
         
     },   500);
 }
+
+
+
+
 
 const loop = setInterval(() => {
     
@@ -28,24 +37,27 @@ const loop = setInterval(() => {
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`;
 
-        mario.src = 'imagens/over.png';
+        mario.src = 'imagens/marioMorto.gif';
         mario.style.width = '75px'
         mario.style.marginLeft = '50px'
-
+        botaoReiniciar.style.display = 'block'//faz com que o botão apareça após a morte do mario (inativo)
         clearInterval(loop);
 
     }
 
 
 }, 10);
-document.addEventListener('keydown', jump);
-pipe.classList.add('animarPipe');
-nuvem.classList.add('animarNuvem');
 
-botaoIniciar.addEventListener('click', function() {
+
+botaoIniciar.addEventListener('click', () => {
+    
     botaoIniciar.style.display = 'none';
-    nuvem.classList.add('animarNuvem');
-    pipe.classList.add('animarPipe');
     document.addEventListener('keydown', jump);
-
+    pipe.classList.add('animarPipe');
+    nuvem.classList.add('animarNuvem');
+    
 })
+
+botaoReiniciar.addEventListener('click', () => {
+    location.reload(); 
+}) //não funciona se a div restart do html estiver em comentario
